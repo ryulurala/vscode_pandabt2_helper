868c5b16cc6f49b8f28424103b49e98bd4ce15f8
ryulurala <ryulurala@gmail.com>
2025-04-30 02:10:54 +0900

    :hammer: Formatter 등록하는 코드 추가

--- main.js
+++ main.js
@@ -9,6 +9,94 @@ async function activate(context) {
     'Congratulations, your extension "pandabt-helper" is now active!'
   );
 
+  registerFormatter(context);
+  registerDefaultTextColor(context);
+}
+
+// This method is called when your extension is deactivated
+async function deactivate() {}
+
+// ### Funcs ###
+async function registerFormatter(context) {
+  const formatter = vscode.languages.registerDocumentFormattingEditProvider(
+    "pandabt",
+    {
+      provideDocumentFormattingEdits(document) {
+        const edits = [];
+        const linesToDelete = []; // 삭제할 줄 번호를 저장하는 배열
+
+        for (let i = 0; i < document.lineCount; i++) {
+          const line = document.lineAt(i);
+          const originalText = line.text;
+
+          const isBlankLine = originalText.trim() === "";
+          const isOnlyCommentMark = originalText.trim() === "//";
+          if (isOnlyCommentMark || isBlankLine) {
+            linesToDelete.push(i);
+
+            continue;
+          }
+
+          // 뒤에 공백 제거
+          const trimmedText = originalText.trimEnd();
+
+          const isCommentLine =
+            trimmedText.trimStart().startsWith("//") ||
+            trimmedText.trimStart().startsWith("/*");
+
+          if (isCommentLine || isBlankLine) continue;
+
+          // 스페이스와 탭을 계산
+          let leadingTabs = 0;
+          let leadingSpaces = 0;
+          for (let j = 0; j < trimmedText.length; j++) {
+            if (trimmedText[j] === "\t") {
+              leadingTabs++;
+            } else if (trimmedText[j] === " ") {
+              leadingSpaces++;
+            } else {
+              break; // 공백과 탭 외를 만나면 계산 멈추기
+            }
+          }
+
+          console.log(
+            `Origin: leadingSpaces: ${leadingSpaces}, leadingTabs: ${leadingTabs}`
+          );
+
+          // 스페이스 개수에 따라 탭 수 결정
+          const spaceToTabCount = Math.floor((leadingSpaces + 1) / 4);
+
+          console.log(
+            `leadingSpaces: ${leadingSpaces} -> tabCount: ${spaceToTabCount}`
+          );
+
+          // 기존 탭을 유지하고 스페이스만 탭으로 변환
+          const newIndent = "\t".repeat(leadingTabs + spaceToTabCount);
+          const newText = newIndent + trimmedText.trimStart();
+
+          if (newText !== originalText) {
+            edits.push(vscode.TextEdit.replace(line.range, newText));
+          }
+        }
+
+        // 삭제할 줄을 반영
+        const sortedLinesToDelete = linesToDelete.sort((a, b) => b - a); // 역순으로 정렬하여 삭제
+
+        sortedLinesToDelete.forEach((lineIndex) => {
+          const line = document.lineAt(lineIndex);
+          edits.push(vscode.TextEdit.delete(line.range)); // 해당 줄을 삭제
+        });
+
+        return edits;
+      },
+    }
+  );
+
+  context.subscriptions.push(formatter);
+}
+
+async function registerDefaultTextColor(context) {
+  // Settings command registration
   const commandApplyDefaultColor = vscode.commands.registerCommand(
     "pandabt-helper.applyDefaultColor",
     () => {
@@ -19,13 +107,6 @@ async function activate(context) {
   );
   context.subscriptions.push(commandApplyDefaultColor);
 
-  initTextColors();
-}
-
-// This method is called when your extension is deactivated
-async function deactivate() {}
-
-async function initTextColors() {
   const config = vscode.workspace.getConfiguration();
   const currentColors = config.get("editor.tokenColorCustomizations") || {};
 
